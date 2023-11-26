babf952f5b22535db133b91aa407bbc2427acc8e
wraps <me@matthieu.pw>
2023-11-26 23:30:22 +0100

    Setup doctrine

--- init-local-keyHeaven.sh
+++ init-local-keyHeaven.sh
@@ -1,5 +1,6 @@
 docker exec php8keyheaven sh -c 'cd keyHeaven/'
 docker exec php8keyheaven sh -c 'cd keyHeaven/ && composer install' && \
+docker exec php8keyheaven sh -c 'cd keyHeaven/ && php bin/console doc:database:create' && \
 docker exec php8keyheaven sh -c 'cd keyHeaven/ && php bin/console doc:mig:mig' && \
 
 # activate only if you know what you do
