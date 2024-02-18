4b1116ccf11f09ce17b75dfdb4b84754d94b2101
wraps <me@matthieu.pw>
2024-02-18 19:35:55 +0100

    Update gameFixture

--- src/DataFixtures/GameFixtures.php
+++ src/DataFixtures/GameFixtures.php
@@ -15,14 +15,19 @@ use App\Repository\EditorRepository;
 use App\Repository\PlatformRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-
-class GameFixtures extends Fixture
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use App\DataFixtures\PlatformFixtures;
+use App\DataFixtures\EditorFixtures;
+use App\DataFixtures\DeveloperFixtures;
+use DateTime;
+class GameFixtures extends Fixture implements DependentFixtureInterface
 {
     private  CategoryRepository $categoryRepository;
     private PlatformRepository $platformRepository;
     private EditorRepository $editorRepository;
     private ConfigurationRepository $configurationRepository;
     private DevelopersRepository $developersRepository;
+    
     public function __construct(CategoryRepository $categoryRepository, PlatformRepository $platformRepository, DevelopersRepository $developersRepository, EditorRepository $editorRepository, ConfigurationRepository $configurationRepository){
         $this->categoryRepository = $categoryRepository;
         $this->platformRepository = $platformRepository;
@@ -30,56 +35,48 @@ class GameFixtures extends Fixture
         $this->configurationRepository = $configurationRepository;
         $this->developersRepository = $developersRepository;
     }
+
+    public function getDependencies()
+    {
+        return [
+            PlatformFixtures::class,
+            EditorFixtures::class,
+            DeveloperFixtures::class,
+        ];
+    }
+
     public function load(ObjectManager $manager)
     {
-        for($i = 1; $i <=10; $i++ ){
-            $category = new Category();
-            $category->setLabel('category ' .$i);
+        $jsonFile = __DIR__ . '/games.json';
+        $gamesData = json_decode(file_get_contents($jsonFile), true);
 
-            $platform = new Platform();
-            $platform->setPlatformName('Platform name '.$i);
-
-            $editor = new Editor();
-            $editor->setName('Editor ' .$i);
-
-            $developer = new Developers();
-            $developer->setName('Developer '.$i);
-
-            $manager->persist($category);
-            $manager->persist($platform);
-            $manager->persist($editor);
-            $manager->persist($developer);
-        }
-
-        $configuration = new Configuration();
-        $configuration->setOperatingSystem("Windows 10/11 with update - 64 bits")
-            ->setProcessor('Intel Core 2 or AMD athlon 64X2')
-            ->setRamMemory('4 GB RAM')
-            ->setGraphicsCard("Intel GMA X4500, NVIDIA GeForce 9600M GT, AMD/ATI Mobility Radeon HD 3650 - requires 256MB VRAM and DirectX 11")
-            ->setDirectX('Version 11')
-            ->setStorage('7 GB available space')
-            ->setAdditionalNotes('Performance scales with higher-end systems.')
-            ->setDisplay('1024x768');
-
-        $manager->persist($configuration);
-        $manager->flush();
-
-        for($i =1; $i <= 10; $i++){
+        foreach ($gamesData as $gameData) {
+            $price = (float) str_replace('€', '', $gameData['price']);
+            $configuration = new Configuration();
+            $configuration->setProcessor($gameData['requiredSpecifications']['processor'])
+                ->setGraphicsCard($gameData['requiredSpecifications']['graphics'])
+                ->setRamMemory($gameData['requiredSpecifications']['memory'])
+                ->setOperatingSystem($gameData['requiredSpecifications']['os'])
+                ->setStorage($gameData['requiredSpecifications']['storage'])
+                ->setDirectX($gameData['requiredSpecifications']['directX'])
+                ->setAdditionalNotes($gameData['requiredSpecifications']['additionalNotes'] ?? '')
+                ->setDisplay($gameData['requiredSpecifications']['display'] ?? '');
+            $manager->persist($configuration);
             $game = new Game();
+            $game->setTitle($gameData['title'])
+                ->setDescription($gameData['description'])
+                ->setPrice($price)
+                ->setExitDate(new DateTime($gameData['releaseDate']))
+                ->setImage($gameData['image'])
+                ->addPlatform($this->platformRepository->findOneBy(['platformName' => $gameData['platform']]) ?? new Platform())
+                ->setDeveloper($this->developersRepository->findOneBy(['name' => $gameData['developer']]) ?? new Developers())
+                ->setEditor($this->editorRepository->findOneBy(['name' => $gameData['publisher']]) ?? new Editor())
+                ->setConfiguration($configuration);
+            
 
-            $game->setTitle('Jeu '.$i)
-                ->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula orci nec justo condimentum bibendum. Sed sodales ac neque vel malesuada. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam mattis tincidunt ultricies. Aliquam metus risus, sodales nec tortor sit amet, eleifend rutrum urna. Pellentesque non massa felis. Nam sodales velit eu consectetur porta. Proin faucibus diam non lobortis lobortis. In in aliquam risus.
-                    Proin sapien diam, tincidunt ut ipsum ullamcorper, viverra pharetra lectus. Cras tristique sit amet ipsum vel tempus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce sit amet interdum ante. Mauris sodales est eget eros facilisis luctus. Donec venenatis tempor arcu, eget bibendum odio dictum vitae. Pellentesque in faucibus augue. Donec in rutrum augue, ultrices lobortis erat.")
-                ->setPrice(22.55)
-                ->setExitDate(new \DateTime())
-                ->setImage('image'.$i.'.png')
-                ->addCategory($this->categoryRepository->findOneBy(['id' => 2]))
-                ->addPlatform($this->platformRepository->findOneBy(['id' => 3]))
-                ->setDeveloper($this->developersRepository->findOneBy(['id' => 1]))
-                ->setConfiguration($this->configurationRepository->findOneBy(['id' => 1]))
-                ->setEditor($this->editorRepository->findOneBy(['id' => 5]));
             $manager->persist($game);
         }
+
         $manager->flush();
     }
 
