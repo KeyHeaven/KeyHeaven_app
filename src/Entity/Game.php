e85b9225c7e7c403b4a99c2a28f00ab572db8044
wraps <me@matthieu.pw>
2024-02-18 19:50:15 +0100

    Add title and exitDate filter in game class

--- src/Entity/Game.php
+++ src/Entity/Game.php
@@ -10,7 +10,12 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
 
+#[ApiFilter(SearchFilter::class, properties : ['title' => 'ipartial'])]
+#[ApiFilter(OrderFilter::class, properties: ['exitDate' => 'DESC'])]
 #[ApiResource(normalizationContext: ['groups' => ['get']])]
 #[Get]
 #[ORM\Entity(repositoryClass: GameRepository::class)]
