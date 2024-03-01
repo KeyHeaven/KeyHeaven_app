22d6a7779378eb04bb070e712b82b0bb1db361dc
wraps <me@matthieu.pw>
2024-03-01 23:59:30 +0100

    Add no-interaction flag for fixture:load

--- init-local-keyHeaven.sh
+++ init-local-keyHeaven.sh
@@ -1,11 +1,10 @@
 docker exec keyheaven_app-php8-1 sh -c 'composer install' && \
-docker exec keyheaven_app-php8-1 sh -c 'php bin/console doc:database:create' && \
 
 docker exec keyheaven_app-php8-1 sh -c 'php bin/console doctrine:migrations:diff' && \
 
 docker exec keyheaven_app-php8-1 sh -c 'php bin/console doc:mig:mig' && \
 
-docker exec keyheaven_app-php8-1 sh -c 'php bin/console doc:fixture:load' && \
+docker exec keyheaven_app-php8-1 sh -c 'php bin/console doc:fixture:load --no-interaction' && \
 
 docker exec keyheaven_app-php8-1 sh -c 'php bin/console lexik:jwt:generate-keypair' && \
 
